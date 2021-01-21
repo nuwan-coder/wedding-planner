2564d0160f855ca284da8347d3da3de563f10a11
Sandeepa <nuwansandeepa3@gmail.com>
2021-01-21 12:07:10 +0530

    design start fragment

---
+++ app/src/main/java/com/example/weddingplanner/StartFragment.kt
@@ -0,0 +1,11 @@
+package com.example.weddingplanner
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StartFragment : Fragment() {
+
+}
