870249b0b3e65c40bfbfe626e4a569744c5c1a9a
Sandeepa <nuwansandeepa3@gmail.com>
2021-01-22 06:47:45 +0530

    complete start fragment

--- app/src/main/java/com/example/weddingplanner/ui/StartFragment.kt
+++ app/src/main/java/com/example/weddingplanner/ui/StartFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weddingplanner.R
 
 class StartFragment : Fragment() {
@@ -16,4 +17,5 @@ class StartFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_start,container,false)
     }
+
 }
